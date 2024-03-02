cc3f4cc538c8d1ba5e173ff328fe02a1b05dbcc5
guilherme.felipe <guilherme.felipe@retta.com.br>
2024-03-02 16:54:28 -0300

    [Chat]  - colocando a função __str__ para que os nomes de usuários e as mensagens sejam  visíveis no painel admin

--- djangoapp/chat/models.py
+++ djangoapp/chat/models.py
@@ -2,19 +2,25 @@ from django.db import models
 from django.conf import settings
 
 class Room(models.Model):
-  user = models.ForeignKey(
-    settings.AUTH_USER_MODEL,
-    on_delete=models.CASCADE,
-  )
-  title = models.CharField(max_length=200)
-  message = models.ManyToManyField('Message')
-  created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+      settings.AUTH_USER_MODEL,
+      on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=200)
+    message = models.ManyToManyField('Message')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+      return self.title
 
 
 class Message(models.Model):
-  user = models.ForeignKey(
-    settings.AUTH_USER_MODEL,
-    on_delete=models.CASCADE,
-  )
-  text = models.TextField()
-  created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+      settings.AUTH_USER_MODEL,
+      on_delete=models.CASCADE,
+    )
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) :
+      return f'{self.user.username}: {self.text}'
