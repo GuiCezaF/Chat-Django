f6af6ab295e8d2019f9ae8e8f241153f7ad9cafa
guilherme.felipe <guilherme.felipe@retta.com.br>
2024-03-09 13:19:01 -0300

    [chat] - adicionado os templates e incluído no settings
    +  criando a primeira view

--- djangoapp/chat/views.py
+++ djangoapp/chat/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Message, Room
 
 def home(request):
-  return HttpResponse("Hello World")
+  
+  rooms = Room.objects.all()
+  messages = Message.objects.all()
+  
+  
+  return render(request, 'index.html', {
+    'rooms': rooms,
+    'messages': messages
+    })
